30eb1a11aa99393505fae0ce31d5854ba0f7fdb3
NOOBNUBY <noobnuby77@gmail.com>
2023-10-02 01:17:50 +0900

    :bug: DropItem Bug

--- src/main/kotlin/com/noobnuby/plugin/events/MobSpawn.kt
+++ src/main/kotlin/com/noobnuby/plugin/events/MobSpawn.kt
@@ -1,5 +1,6 @@
 package com.noobnuby.plugin.events
 
+import org.bukkit.entity.EntityType
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.entity.EntitySpawnEvent
@@ -7,6 +8,7 @@ import org.bukkit.event.entity.EntitySpawnEvent
 class MobSpawn: Listener {
     @EventHandler
     fun doMobSpawning(e: EntitySpawnEvent) {
+        if (e.entityType == EntityType.DROPPED_ITEM) return
         e.isCancelled = true
     }
 }
