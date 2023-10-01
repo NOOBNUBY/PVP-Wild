0313c6105f2a1cf0dcfd08425b9635b63a466439
NOOBNUBY <noobnuby77@gmail.com>
2023-10-02 01:18:45 +0900

    Variable

--- src/main/kotlin/com/noobnuby/plugin/events/PlayerChat.kt
+++ src/main/kotlin/com/noobnuby/plugin/events/PlayerChat.kt
@@ -2,12 +2,14 @@ package com.noobnuby.plugin.events
 
 import com.noobnuby.plugin.utils.Variable
 import io.papermc.paper.event.player.AsyncChatEvent
+import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 
 class PlayerChat: Listener {
+    @EventHandler
     fun onPlayerChat(e: AsyncChatEvent) {
-        if(Variable().ChatEnable == false) {
-            e.isCancelled = false
+        if(Variable.ChatEnable == false) {
+            e.isCancelled = true
         }
     }
 }
