241df14c69c44b9fedfe5f20289fe8f6048867dc
NOOBNUBY <noobnuby77@gmail.com>
2023-10-01 11:22:40 +0900

    :sparkles: Add player killMessage

--- src/main/kotlin/com/noobnuby/plugin/events/Death.kt
+++ src/main/kotlin/com/noobnuby/plugin/events/Death.kt
@@ -1,5 +1,6 @@
 package com.noobnuby.plugin.events
 
+import net.kyori.adventure.text.Component
 import org.bukkit.GameMode
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
@@ -9,6 +10,11 @@ class Death: Listener {
     @EventHandler
     fun onDeath(e: PlayerDeathEvent) {
         val p = e.entity
+        val k = p.killer
         p.gameMode = GameMode.SPECTATOR
+
+        if(k != null) {
+            e.deathMessage(Component.text("${k.name} 님이 ${p.name} 님을 처치했습니다!"))
+        }
     }
 }
