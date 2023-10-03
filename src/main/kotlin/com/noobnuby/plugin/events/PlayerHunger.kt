fbfcddba8b0f3a345748999ab0f4079472392dc1
NOOBNUBY <noobnuby77@gmail.com>
2023-10-03 21:38:21 +0900

    :bug: Fix EventHandler

--- src/main/kotlin/com/noobnuby/plugin/events/PlayerHunger.kt
+++ src/main/kotlin/com/noobnuby/plugin/events/PlayerHunger.kt
@@ -1,10 +1,12 @@
 package com.noobnuby.plugin.events
 
 import com.noobnuby.plugin.utils.Variable
+import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.entity.FoodLevelChangeEvent
 
 class PlayerHunger:Listener {
+    @EventHandler
     fun onPlayerHunger(e: FoodLevelChangeEvent) {
         if(Variable.isKillTimeStart == false) {
             e.isCancelled = true
