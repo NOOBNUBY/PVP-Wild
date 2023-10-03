49f8568993e810d5f7f74993b82a4781a9ae8de2
NOOBNUBY <noobnuby77@gmail.com>
2023-10-04 00:23:50 +0900

    player get damage false

--- src/main/kotlin/com/noobnuby/plugin/events/PlayerDamage.kt
+++ src/main/kotlin/com/noobnuby/plugin/events/PlayerDamage.kt
@@ -2,10 +2,12 @@ package com.noobnuby.plugin.events
 
 import com.noobnuby.plugin.utils.Variable
 import org.bukkit.entity.Player
+import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.entity.EntityDamageEvent
 
 class PlayerDamage:Listener {
+    @EventHandler
     fun onPlayerDamage(e: EntityDamageEvent) {
         if(e.entity is Player) {
             if(Variable.isKillTimeStart == false) {
