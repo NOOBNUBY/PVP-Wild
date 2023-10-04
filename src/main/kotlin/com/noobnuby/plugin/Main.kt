b53c4e9de7c9b86b41ac8f15c4e660550903c910
NOOBNUBY <noobnuby77@gmail.com>
2023-10-04 19:56:28 +0900

    :art: Improve gamerule

--- src/main/kotlin/com/noobnuby/plugin/Main.kt
+++ src/main/kotlin/com/noobnuby/plugin/Main.kt
@@ -3,8 +3,10 @@ package com.noobnuby.plugin
 import com.noobnuby.plugin.AutoSmelt.OreBreak
 import com.noobnuby.plugin.events.*
 import com.noobnuby.plugin.init.Schem
+import com.noobnuby.plugin.init.GameRule
 import com.noobnuby.plugin.utils.SetUpKommand
 import io.github.monun.kommand.kommand
+import org.bukkit.Bukkit.getWorld
 import org.bukkit.plugin.java.JavaPlugin
 
 class Main : JavaPlugin() {
@@ -16,7 +18,6 @@ class Main : JavaPlugin() {
     //TODO : 스코어보드
     //TODO : 명령어 방지
     //TODO : 몹 스폰 방지 이벤트 개선
-    //TODO : 인첸트 테이블 청금석 없이도 되게
     //TODO : 공속제거
     override fun onEnable() {
         instance = this
@@ -28,16 +29,16 @@ class Main : JavaPlugin() {
             registerEvents(Death(), this@Main)
             registerEvents(BreakBlock(), this@Main)
             registerEvents(MobSpawn(), this@Main)
-            registerEvents(WeatherChange(), this@Main)
+            //registerEvents(WeatherChange(), this@Main)
             registerEvents(BlockOnPlace(), this@Main)
             registerEvents(PlayerChat(), this@Main)
             registerEvents(OreBreak(),this@Main)
             registerEvents(PlayerDamage(),this@Main)
             registerEvents(PlayerHunger(),this@Main)
-            registerEvents(DayChange(),this@Main)
             registerEvents(EnchantTable(),this@Main)
         }
 
+        GameRule().setGameRule()
         Schem().loadSchem()
         SetUpKommand.setupKommand()
     }
---
+++ src/main/kotlin/com/noobnuby/plugin/init/GameRule.kt
@@ -0,0 +1,16 @@
+package com.noobnuby.plugin.init
+
+import org.bukkit.Bukkit
+import org.bukkit.GameRule
+
+class GameRule {
+    fun setGameRule() {
+        val world = Bukkit.getWorld("world")
+
+        world?.time = 1000
+        world?.setStorm(false)
+
+        world?.setGameRule(GameRule.DO_DAYLIGHT_CYCLE,false)
+        world?.setGameRule(GameRule.DO_WEATHER_CYCLE,false)
+    }
+}
