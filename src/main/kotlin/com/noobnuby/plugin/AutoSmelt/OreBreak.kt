7d62912e09b54e065ddd977fa1c699febf74910d
NOOBNUBY <noobnuby77@gmail.com>
2023-10-08 21:13:32 +0900

    :bug: Fix Drop amount

--- src/main/kotlin/com/noobnuby/plugin/AutoSmelt/OreBreak.kt
+++ src/main/kotlin/com/noobnuby/plugin/AutoSmelt/OreBreak.kt
@@ -1,6 +1,5 @@
 package com.noobnuby.plugin.AutoSmelt
 
-import com.noobnuby.plugin.utils.Variable
 import org.bukkit.GameMode
 import org.bukkit.Material
 import org.bukkit.enchantments.Enchantment
@@ -8,28 +7,39 @@ import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.block.BlockBreakEvent
 import org.bukkit.inventory.ItemStack
+import java.util.*
 
-class OreBreak: Listener {
+class OreBreak : Listener {
     @EventHandler
     fun onBlockBreak(e: BlockBreakEvent) {
         val p = e.player
         val block = e.block.type
         val hand: ItemStack = e.player.inventory.itemInMainHand
+        val rd = Random()
 
-        if(p.gameMode != GameMode.SURVIVAL) return
-        if (hand.containsEnchantment(Enchantment.SILK_TOUCH)) return
+        if (!hand.containsEnchantment(Enchantment.LOOT_BONUS_BLOCKS)) return
+        if (p.gameMode != GameMode.SURVIVAL) return
+        e.isDropItems = false
 
-        when {
-            block == Material.IRON_ORE || block == Material.GOLD_ORE || block == Material.EMERALD_ORE -> {
-                e.isDropItems = false
 
-                for (i in e.block.getDrops(hand)) {
-                    if (block.name.endsWith("IRON_ORE")) i.type = Material.IRON_INGOT
-                    if (block.name.endsWith("GOLD_ORE")) i.type = Material.GOLD_INGOT
-                    if (block.name.endsWith("EMERALD_ORE")) i.type = Material.DIAMOND
+        if (block == Material.IRON_ORE || block == Material.GOLD_ORE || block == Material.EMERALD_ORE) {
+            var amount = 1
 
-                    e.block.world.dropItemNaturally(e.block.location, i)
-                }
+            when (hand.getEnchantmentLevel(Enchantment.LOOT_BONUS_BLOCKS)) {
+                1 -> if (rd.nextDouble() <= 0.33) amount += 1
+                2 -> if (rd.nextDouble() <= 0.50) amount += (rd.nextInt(2) + 1)
+                3 -> if (rd.nextDouble() <= 0.60) amount += (rd.nextInt(3) + 1)
+            }
+
+            var stack: ItemStack = when (block) {
+                Material.IRON_ORE -> ItemStack(Material.IRON_INGOT)
+                Material.GOLD_ORE -> ItemStack(Material.GOLD_INGOT)
+                Material.EMERALD_ORE -> ItemStack(Material.DIAMOND)
+                else -> return
+            }
+
+            for (i: Int in 1..amount) {
+                e.block.world.dropItemNaturally(e.block.location, stack)
             }
         }
     }
